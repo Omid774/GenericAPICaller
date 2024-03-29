d92ef4cd14e31c3632b5fe4c8211c42de393a466
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-29 10:30:38 +0330

    Initial Commit

---
+++ GenericAPICaller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericAPICaller
+//
+//  Created by Omid Heydarzadeh on 3/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
